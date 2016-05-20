1b3782d6996f3796a8b14e54dded67b42b0e7744
21319471 <bandymyeni@gmail.com>
2016-05-20 15:00:20 -0600

    payment Deatils Print (PDF_File)

--- GFS/Controllers/PaymentsController.cs
+++ GFS/Controllers/PaymentsController.cs
@@ -7,10 +7,16 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using GFS.Models;
-
 using System.Web.Helpers;
 using System.Text;
 using System.Net.Mail;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.draw;
+using System.Drawing;
+using iTextSharp.text.html.simpleparser;
+using Font = iTextSharp.text.Font;
+using System.IO;
 
 namespace GFS.Controllers
 {
@@ -33,6 +39,138 @@ namespace GFS.Controllers
             }
             return View(pay);
         }
+
+        public FileStreamResult PrintList()
+        {
+            //Set up the document and the MS to write it to and create the PDF writer instance
+            MemoryStream memStream = new MemoryStream();
+            // Create a Document object
+            var document = new Document(PageSize.A4, 0, 0, 0, 0);
+
+            // Create a new PdfWriter object, specifying the output stream
+            var output = new MemoryStream();
+            var writer = PdfWriter.GetInstance(document, output);
+
+            // Open the Document for writing
+            document.Open();
+
+            //Set up fonts used in the document
+            Font fontHeading3 = FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.BOLD + Font.UNDERLINE, BaseColor.BLACK);
+            Font subHeaderFont = FontFactory.GetFont(FontFactory.TIMES_ROMAN, 10, Font.BOLDITALIC, BaseColor.BLACK);
+            Font PolicyDetailFont = FontFactory.GetFont(FontFactory.TIMES_ROMAN, 8, Font.BOLD, BaseColor.BLACK);
+            Font fontBody = FontFactory.GetFont(FontFactory.TIMES_ITALIC, 10, Font.BOLD, BaseColor.BLACK);
+            Font thFont = FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.BOLD, BaseColor.BLACK);
+            Font redFont = FontFactory.GetFont(FontFactory.COURIER_BOLDOBLIQUE, 18, Font.BOLD, BaseColor.RED);
+            Font fontData = FontFactory.GetFont(FontFactory.COURIER_OBLIQUE, 12, BaseColor.BLACK);
+
+
+            //Open the PDF document
+            document.Open();
+
+            PdfPTable tblHeader = new PdfPTable(3)
+            {
+                SpacingBefore = 50f,
+                SpacingAfter = 50f
+            };
+
+            PdfPCell hCell6 = new PdfPCell(new Phrase("\nGumbi Financial Services\n", fontHeading3))
+            {
+                HorizontalAlignment = 1,
+                Colspan = 3,
+                Border = 0
+            };
+
+            PdfPCell hCell7 = new PdfPCell(new Phrase("Gumbi Financial Services, Durban, South Africa", PolicyDetailFont))
+            {
+                Border = 0,
+                Colspan = 3,
+                HorizontalAlignment = 1
+            };
+
+            PdfPCell hCell8 = new PdfPCell(new Phrase("Tel: 031-459-7500 / Fax: 031-459-7600", PolicyDetailFont))
+            {
+                Border = 0,
+                Colspan = 3,
+                HorizontalAlignment = 1
+            };
+            PdfPCell hCell9 = new PdfPCell(new Phrase("Email :Info@gumbi.com / Enquiry@gumbi.com", PolicyDetailFont))
+            {
+                Colspan = 3,
+                Border = 0,
+                HorizontalAlignment = 1
+            };
+
+
+
+            tblHeader.AddCell(hCell6);
+            tblHeader.AddCell(hCell7);
+            tblHeader.AddCell(hCell8);
+            tblHeader.AddCell(hCell9);
+
+            document.Add(tblHeader);
+
+            //Get individual Payment Details for a Member
+            var obj = db.Payments.ToList().FindLast(x => x.policyNo == Session["det"].ToString());
+
+            var orderInfoTable = new PdfPTable(1);
+            orderInfoTable.HorizontalAlignment = Element.ALIGN_CENTER;
+            orderInfoTable.SpacingBefore = 40;
+            orderInfoTable.SpacingAfter = 50;
+            orderInfoTable.DefaultCell.Border = 0;
+            orderInfoTable.WidthPercentage = 80;
+
+
+
+            PdfPCell tc1 = new PdfPCell(new Phrase("Policy No: " + obj.policyNo, fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc1);
+            PdfPCell tc2 = new PdfPCell(new Phrase("Customer Name:" + obj.CustomerName, fontData)) { Indent = 5, Border = 0 };
+            orderInfoTable.AddCell(tc2);
+            PdfPCell tc3 = new PdfPCell(new Phrase("Policy Plan:" + obj.plan, fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc3);
+            PdfPCell tc4 = new PdfPCell(new Phrase("Amount Due:" + Convert.ToString(obj.dueAmount), fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc4);
+            PdfPCell tc5 = new PdfPCell(new Phrase("Amount Paid:" + Convert.ToString(obj.amount), fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc5);
+            PdfPCell tc6 = new PdfPCell(new Phrase("Out Standing Amount:" + Convert.ToString(obj.outstandingAmount), fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc6);
+            PdfPCell tc7 = new PdfPCell(new Phrase("Date :" + DateTime.Now.ToString(), fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc7);
+            PdfPCell tc8 = new PdfPCell(new Phrase("Captured By:" + obj.cashierName, fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc8);
+            PdfPCell tc9 = new PdfPCell(new Phrase("Branch: " + obj.branch, fontData)) { Indent = 5, Border = 0, Colspan = 2, };
+            orderInfoTable.AddCell(tc9);
+
+
+
+            var paragraph = new Paragraph("Payment Details Receipt", redFont);
+            paragraph.Alignment = Element.ALIGN_CENTER;
+            document.Add(paragraph);
+            document.Add(orderInfoTable);
+
+            Paragraph gap = new Paragraph("\n\n");
+
+            document.Add(gap);
+
+            PdfPTable tblOfficeDetail = new PdfPTable(2);
+            PdfPCell signCell = new PdfPCell(new Phrase("Signature: .....................................", fontBody)) { Colspan = 2, HorizontalAlignment = 3, Border = 0 };
+            tblOfficeDetail.AddCell(signCell);
+            PdfPCell dateCell = new PdfPCell(new Phrase("Date Issued:............/............/..........", fontBody)) { HorizontalAlignment = 3, Border = 0 };
+            tblOfficeDetail.AddCell(dateCell);
+            PdfPCell stampCell = new PdfPCell(new Phrase("Stamp  :.......................................", fontBody)) { Rowspan = 2, HorizontalAlignment = 3, Border = 0 };
+            tblOfficeDetail.AddCell(stampCell);
+
+            document.Add(tblOfficeDetail);
+            var logo = iTextSharp.text.Image.GetInstance(Server.MapPath("~/Images/Logo.png"));
+            logo.Alignment = Element.ALIGN_CENTER; // Absolute position
+            document.Add(logo);
+            document.Close();
+
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("Content-Disposition", string.Format("attachment;filename=Receipt-{0}.pdf", obj.policyNo));
+            Response.BinaryWrite(output.ToArray());
+            return new FileStreamResult(memStream, "application/pdf");
+        }
+
 
         // GET: Payments/Details/5
         public ActionResult Details(int? id)
